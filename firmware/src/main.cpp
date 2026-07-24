c875369cb517d87b0f8a3ec00d0f3b5bfc765637
thomasway-sketch <thomas.way14@gmail.com>
2026-07-24 22:32:09 +0100

    Changed the main.cpp file in the firmware directory to make the motor spin forward and backwards at 5 second intervals for the purpose of testing the motors

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,21 +1,51 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+#define PWMA 22
+#define PWMA_CHANNEL 0
+#define PWMB 23
+#define PWMB_CHANNEL 1
+#define AIN1 21
+#define AIN2 19
+#define BIN1 18
+#define BIN2 4
+#define STBY 27
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+
   Serial.begin(115200);
+
+  pinMode(PWMA, OUTPUT);
+  pinMode(PWMB, OUTPUT);
+  pinMode(AIN1, OUTPUT);
+  pinMode(AIN2, OUTPUT);
+  pinMode(BIN1, OUTPUT);
+  pinMode(BIN2, OUTPUT);
+  pinMode(STBY, OUTPUT);
+
+
+  ledcSetup(PWMB_CHANNEL, 20000, 8); 
+  ledcAttachPin(PWMB, PWMB_CHANNEL);            
+
+  digitalWrite(STBY, HIGH);
+ 
+
+  ledcWrite(PWMB_CHANNEL, 128);  
+  
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  Serial.println("hello from ESP32 gang");
-  delay(1000);
+  // put your main code here, to run repeatedly:""
+  Serial.println("BIN1 high and BIN2 low");
+  digitalWrite(BIN1, HIGH);
+  digitalWrite(BIN2, LOW);
+
+  delay(5000);
+
+  Serial.println("BIN1 low and BIN2 high");
+  digitalWrite(BIN1, LOW);
+  digitalWrite(BIN2, HIGH);
+
+  delay(5000);
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
