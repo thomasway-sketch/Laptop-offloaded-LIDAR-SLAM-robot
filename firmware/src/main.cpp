51cb2b944459662bb099f668d21d047c98d41c82
thomasway-sketch <thomas.way14@gmail.com>
2026-07-26 13:46:47 +0100

    I created a simple ros2 subscriber which subscribes to cmd_vel and sends that data over UDP to the same port the ESP32 collects data from. Set up the ESP32 to read data from port 8889 and connect to my WiFi network. Opted for this instead of making the ESP32 an access point due to convinience with having to switch networks to download packages.

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -1,4 +1,7 @@
 #include <Arduino.h>
+#include <WiFi.h>
+#include <WiFiUdp.h>
+#include "credentials.cpp"
 
 #define PWMA 22
 #define PWMA_CHANNEL 0
@@ -9,6 +12,15 @@
 #define BIN1 18
 #define BIN2 4
 #define STBY 27
+
+
+#define currentPort 8889
+
+WiFiUDP wifiUDP;
+int character;
+String msg;
+float linear;
+float angular;
 
 void setup() {
   // put your setup code here, to run once:
@@ -26,17 +38,30 @@ void setup() {
 
   ledcSetup(PWMB_CHANNEL, 20000, 8); 
   ledcAttachPin(PWMB, PWMB_CHANNEL);            
+  ledcWrite(PWMB_CHANNEL, 128); 
 
   digitalWrite(STBY, HIGH);
- 
-
-  ledcWrite(PWMB_CHANNEL, 128);  
   
+  WiFi.begin(ssid, password);
+
+  IPAddress local_IP(192, 168, 4, 210);
+  IPAddress gateway(192, 168, 4, 1);     // your router's address
+  IPAddress subnet(255, 255, 255, 0);
+  WiFi.config(local_IP, gateway, subnet);
+
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(500);
+    Serial.print(".");
+  }
+
+  wifiUDP.begin(currentPort);
+
+
 }
 
 void loop() {
   // put your main code here, to run repeatedly:""
-  Serial.println("BIN1 high and BIN2 low");
+  /*Serial.println("BIN1 high and BIN2 low");
   digitalWrite(BIN1, HIGH);
   digitalWrite(BIN2, LOW);
 
@@ -46,6 +71,23 @@ void loop() {
   digitalWrite(BIN1, LOW);
   digitalWrite(BIN2, HIGH);
 
-  delay(5000);
+  delay(5000);*/
+
+  wifiUDP.parsePacket();
+  character = wifiUDP.read();
+  while(character != -1) {
+    msg += (char)character;
+    character = wifiUDP.read();
+  }
+
+  linear = msg.substring(0, msg.indexOf(",")).toFloat();
+  angular = msg.substring(msg.indexOf(",") + 1).toFloat();
+
+  Serial.println(msg);
+  Serial.println(linear);
+  Serial.println(angular);
+
+  msg = "";
+  delay(2000);
 }
 
---
+++ ros2_ws/src/robot_bridge/robot_bridge/robot_bridge.py
@@ -0,0 +1,40 @@
+import rclpy
+from rclpy.node import Node
+from geometry_msgs.msg import TwistStamped
+import socket
+
+
+class RobotBridge(Node):
+
+    def __init__(self):
+        super().__init__('robot_bridge')
+        self.subscription = self.create_subscription(
+            TwistStamped,
+            'cmd_vel',
+            self.bridge_publish,
+            10)
+        self.subscription  # prevent unused variable warning
+        self.UDP_IP = "192.168.4.210"
+        self.UDP_PORT = 8889
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    def bridge_publish(self, msg):
+        data = f"{msg.twist.linear.x}, {msg.twist.angular.z}"
+        self.sock.sendto(data.encode(), (self.UDP_IP, self.UDP_PORT))
+
+        
+
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    robot_bridge = RobotBridge()
+
+    rclpy.spin(robot_bridge)
+
+    robot_bridge.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == '__main__':
+    main()
